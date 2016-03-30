b74b09c486166bc819a3e42fa3e0eff67602ab28
jayant7k <jayant7k@gmail.com>
2016-03-30 10:39:58 +0530

    adding elasticsearch into weather tweet

--- progs/tweetWeather.py
+++ progs/tweetWeather.py
@@ -1,10 +1,19 @@
 # pip install tweepy 
+# pip install elasticsearch
+
 import tweepy
+from datetime import datetime
+from elasticsearch import Elasticsearch
 
 def get_api(cfg):
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
+
+def get_es():
+  es = Elasticsearch()
+  res = es.search(index="", body={"query": {"match"}})
+
 
 def main():
   # Fill in the values noted in previous step here
