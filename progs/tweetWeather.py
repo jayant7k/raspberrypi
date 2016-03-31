709255ae46f959bc39b78915fff92588ed3fcf13
jayant.kumar <jayant.kumar@naukri.com>
2016-03-31 10:23:52 +0530

    adding tweet weather for tweeting weather updates to #raspberrypi

--- progs/tweetWeather.py
+++ progs/tweetWeather.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 # pip install tweepy 
 # pip install elasticsearch
 
+import json
 import tweepy
 from datetime import datetime
 from elasticsearch import Elasticsearch
@@ -11,8 +13,29 @@ def get_api(cfg):
   return tweepy.API(auth)
 
 def get_es():
+  d = datetime.now()
+  dt = d.strftime("%Y-%m-%dT%H:00:00Z")
+  qry = {
+    "query": {
+      "range": {
+        "dt": {
+          "gte": dt
+        }
+      }
+    },
+    "sort": {
+      "dt": {
+        "order": "desc"
+      }
+    },
+    "from": 0,
+    "size": 1
+  }
   es = Elasticsearch()
-  res = es.search(index="", body={"query": {"match"}})
+  res = es.search(index="ieth", body=qry)
+  hit = res['hits']['hits']
+  return hit
+  
 
 
 def main():
@@ -23,9 +46,22 @@ def main():
     "access_token"        : "KEY",
     "access_token_secret" : "KEY" 
     }
+  tmp = get_es()
+  it = tmp[0]['_source']['it']
+  et = tmp[0]['_source']['et']
+  ih = tmp[0]['_source']['ih']
+  eh = tmp[0]['_source']['eh']
+  dt = tmp[0]['_source']['dt']
+  print("%s => it: %s, et: %s, ih: %s, eh: %s" % (dt, it, et, ih, eh))
 
+  t = datetime.now()
+  tm = t.strftime("%I:%M %p")
+
+  tweet = "Weather Today at {}: temperature(room={}°C, outside={}°C), humidity(room={}%, outside={}%) #raspberrypi".format(tm,it,et,ih,eh)
+  print tweet
+  # print tmp['et']
   api = get_api(cfg)
-  tweet = "Hello, world! from tweepy"
+  # tweet = "Hello, world! from tweepy"
   status = api.update_status(status=tweet) 
   # Yes, tweet is called 'status' rather confusing
 
